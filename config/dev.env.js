d1b2deec662898ab3bd6da86ae9378b434feaea0
Erik Isidore <ericssonico@protonmail.ch>
2017-03-03 09:47:34 -0300

    Implemented environment configuration files
    - A new directory, config, have been added to the root of the
    project, this directory contains the base files that will be used
    by our Webpack configuration files to properly separate and setup
    build and development environments.

---
+++ config/dev.env.js
@@ -0,0 +1,4 @@
+export default {
+  NODE_ENV: '"development"',
+  PORT: 5000
+}
---
+++ config/index.js
@@ -0,0 +1,23 @@
+import path from 'path';
+
+import devEnv from './dev.env';
+import prodEnv from './prod.env';
+
+export default {
+  build: {
+    env: prodEnv,
+    assetsRoot: path.resolve(__dirname, '../app/assets'),
+    assetsSubDirectory: '.',
+    assetsPublicPath: path.resolve(__dirname, '..app/assets/'),
+    cssSourceMap: true
+  },
+
+  dev: {
+    env: devEnv,
+    port: devEnv['PORT'] || 8080,
+    assetsRoot: path.resolve(__dirname, '../app/assets'),
+    assetsSubDirectory: '.',
+    assetsPublicPath: `http://localhost:${devEnv.PORT || 8080}/assets/`,
+    cssSourceMap: true
+  }
+}
