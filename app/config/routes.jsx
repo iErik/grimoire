0fdbdb5d4dc1821e4de6431190bb2d17ebde9b29
Erik Isidore <ericssonico@protonmail.ch>
2017-03-27 15:33:08 -0300

    Added configuration module files to app directory
    - A new directory config/ has been added to the app/ directory,
    this directory holds base configuration files for our app such as
    the router configuration and the initial definitions for the app
    settings.

---
+++ app/config/routes.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Route, IndexRoute, IndexRedirect } from 'react-router';
+
+import RootLayout from 'layouts/RootLayout';
+import DashboardLayout from 'containers/layouts/DashboardLayout';
+import EditorLayout from 'containers/layouts/EditorLayout';
+
+import NotesPage from 'pages/NotesPage';
+import NotebooksPage from 'pages/NotebooksPage';
+import SettingsPage from 'pages/SettingsPage';
+import EditorPage from 'pages/EditorPage'
+
+export default (
+  <Route path="/" component={ RootLayout }>
+    <IndexRedirect to="dashboard" />
+
+    <Route path="dashboard" component={ DashboardLayout }>
+      <IndexRoute component={ NotesPage } />
+      <Route path="notebooks" component={ NotebooksPage } />
+      <Route path="settings" component={ SettingsPage } />
+    </Route>
+
+    <Route path="editor/:notebookId/:noteId" component={ EditorLayout }>
+      <IndexRoute component={ EditorPage } />
+    </Route>
+  </Route>
+);
