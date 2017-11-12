8376d46badf0b290736eba7f943b3413e953e63d
Erik Isidore <ericssonico@protonmail.ch>
2017-11-12 20:10:57 -0200

    Replace NoteViewer's title p tag with dynamic input
    The NoteViewer component now allows the user to change the selected
    note's title

--- app/pages/NotesPage/components/NoteViewer/NoteViewer.jsx
+++ app/pages/NotesPage/components/NoteViewer/NoteViewer.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
 import moment from 'moment';
 
+import Input from 'components/Input';
+
 export default class NoteViewer extends React.Component {
+  renameNote(ev) {
+    this.props.updateNoteTitle(this.props.selectedNote, ev.target.value);
+  }
+
   renderCreationDate() {
     if (this.props.selectedNote.createdAt) {
       return moment(this.props.selectedNote.createdAt).calendar(null, {
@@ -25,12 +31,12 @@ export default class NoteViewer extends React.Component {
       <div className="note-viewer">
         <div className="note-header">
           <div className="note-title">
-            <p className="title">
-              { this.props.isStoreLoading
-              ? ''
-              : this.props.selectedNote.title
-              }
-            </p>
+            <Input
+              type="text"
+              className="input-title"
+              value={ this.props.isStoreLoading ? '' : this.props.selectedNote.title }
+              onChange={ this.renameNote.bind(this) }
+            />
           </div>
           <div className="note-meta">
             <p className="meta-date">
