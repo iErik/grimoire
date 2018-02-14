8e4a32269964d0f39121d0b148fce0256ef4dce8
Erik Isidore <ericssonico@protonmail.ch>
2018-02-14 13:59:40 -0200

    Add platform css class to RootLayout

--- app/layouts/RootLayout/RootLayout.jsx
+++ app/layouts/RootLayout/RootLayout.jsx
@@ -1,6 +1,7 @@
 import { remote } from 'electron';
 import React from 'react';
 import ui from 'redux-ui';
+import os from 'os';
 
 @ui({
   state: {
@@ -9,8 +10,10 @@ import ui from 'redux-ui';
 })
 export default class RootLayout extends React.Component {
   render() {
+    let platform = os.platform();
+
     return (
-      <div className={ `root-container theme-${this.props.appTheme}` }>
+      <div className={`root-container ${ platform } theme-${this.props.appTheme}`}>
         { this.props.children }
       </div>
     );
